e1100da280483529aad9ba8cf0002652b48abc48
Lazzatios <lseiilova@nu.edu.kz>
2021-07-26 13:49:44 +0600

    feat: add double tap gesture recognizer

--- GesturesAppcoda/TapViewController.m
+++ GesturesAppcoda/TapViewController.m
@@ -9,6 +9,7 @@
 
 @interface TapViewController ()
 - (void) handleSingleTapGesture: (UITapGestureRecognizer *)tapGestureRecognizer;
+- (void) handleDoubleTapGesture: (UITapGestureRecognizer *)tapGestureRecognizer;
 @end
 
 @implementation TapViewController
@@ -17,8 +18,13 @@
     [super viewDidLoad];
     
     UITapGestureRecognizer *singleTapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTapGesture:)];
-    
+    singleTapGesture.numberOfTapsRequired = 1;
     [self.testView addGestureRecognizer:singleTapGesture];
+    
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTapGesture:)];
+    doubleTapRecognizer.numberOfTapsRequired = 2;
+    doubleTapRecognizer.numberOfTouchesRequired = 2;
+    [self.testView addGestureRecognizer:doubleTapRecognizer];
 }
 
 - (void) handleSingleTapGesture:(UITapGestureRecognizer *)tapGestureRecognizer {
@@ -34,6 +40,19 @@
     self.testView.frame = CGRectMake(self.testView.frame.origin.x, self.testView.frame.origin.y, newWidth, self.testView.frame.size.height);
     self.testView.center = currentCenter;
     
+}
+
+- (void) handleDoubleTapGesture:(UITapGestureRecognizer *)tapGestureRecognizer {
+    CGSize newSize = CGSizeMake(100.0, 100.0);
+    if (self.testView.frame.size.width == 100.0 && self.testView.frame.size.height == 100.0) {
+        newSize.width = 200.0;
+        newSize.height = 200.0;
+    }
+    
+    CGPoint currentCenter = self.testView.center;
+    
+    self.testView.frame = CGRectMake(self.testView.frame.origin.x, self.testView.frame.origin.y, newSize.width, newSize.height);
+    self.testView.center = currentCenter;
 }
 
 @end
